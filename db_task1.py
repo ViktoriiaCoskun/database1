a2b5aca408d4ce4fd20164ec103a20ddbab08668
ViktoriiaCoskun <120759932+ViktoriiaCoskun@users.noreply.github.com>
2023-03-07 13:30:57 +0100

    Add files via upload

---
+++ db_task1.py
@@ -0,0 +1,174 @@
+import sqlite3
+
+def create_connection(db_file):
+   """ create a database connection to the SQLite database
+       specified by db_file
+   :param db_file: database file
+   :return: Connection object or None
+   """
+   conn = None
+   try:
+       conn = sqlite3.connect(db_file)
+       return conn
+   except sqlite3.Error as e:
+       print(e)
+   return conn
+
+def add_project(conn, projekt):
+   """
+   Create a new project into the projects table
+   :param conn:
+   :param projekt:
+   :return: project id
+   """
+   sql = '''INSERT INTO projects(name, start_date, end_date)
+             VALUES(?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, project)
+   conn.commit()
+   return cur.lastrowid
+
+def add_task(conn, task):
+   """
+   Create a new task into the tasks table
+   :param conn:
+   :param task:
+   :return: task id
+   """
+   sql = '''INSERT INTO tasks(project_id, name, description, status, start_date, end_date)
+             VALUES(?,?,?,?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, task)
+   conn.commit()
+   return cur.lastrowid
+
+def select_task_by_status(conn, status):
+   """
+   Query tasks by priority
+   :param conn: the Connection object
+   :param status:
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute("SELECT * FROM tasks WHERE status=?", (status,))
+
+   rows = cur.fetchall()
+   return rows
+
+def select_all(conn, table):
+   """
+   Query all rows in the table
+   :param conn: the Connection object
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute(f"SELECT * FROM {table}")
+   rows = cur.fetchall()
+
+   return rows
+
+def select_where(conn, table, **query):
+   """
+   Query tasks from table with data from **query dict
+   :param conn: the Connection object
+   :param table: table name
+   :param query: dict of attributes and values
+   :return:
+   """
+   cur = conn.cursor()
+   qs = []
+   values = ()
+   for k, v in query.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+   cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
+   rows = cur.fetchall()
+   return rows
+
+def update(conn, table, id, **kwargs):
+   """
+   update status, begin_date, and end date of a task
+   :param conn:
+   :param table: table name
+   :param id: row id
+   :return:
+   """
+   parameters = [f"{k} = ?" for k in kwargs]
+   parameters = ", ".join(parameters)
+   values = tuple(v for v in kwargs.values())
+   values += (id, )
+
+   sql = f''' UPDATE {table}
+             SET {parameters}
+             WHERE id = ?'''
+   try:
+       cur = conn.cursor()
+       cur.execute(sql, values)
+       conn.commit()
+       print("OK")
+   except sqlite3.OperationalError as e:
+       print(e)
+
+def delete_where(conn, table, **kwargs):
+   """
+   Delete from table where attributes from
+   :param conn:  Connection to the SQLite database
+   :param table: table name
+   :param kwargs: dict of attributes and values
+   :return:
+   """
+   qs = []
+   values = tuple()
+   for k, v in kwargs.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+
+   sql = f'DELETE FROM {table} WHERE {q}'
+   cur = conn.cursor()
+   cur.execute(sql, values)
+   conn.commit()
+   print("Deleted")
+
+def delete_all(conn, table):
+   """
+   Delete all rows from table
+   :param conn: Connection to the SQLite database
+   :param table: table name
+   :return:
+   """
+   sql = f'DELETE FROM {table}'
+   cur = conn.cursor()
+   cur.execute(sql)
+   conn.commit()
+   print("Deleted")
+
+if __name__ == "__main__":
+   project = ("Repeating English", "2020-05-11 00:00:00", "2020-05-13 00:00:00")
+
+   conn = create_connection("database.db")
+   pr_id = add_project(conn, project)
+
+   task = (
+       pr_id,
+       "right verbs",
+       "remember verbs on page 30",
+       "started",
+       "2020-05-11 12:00:00",
+       "2020-05-11 15:00:00"
+   )
+
+   task_id = add_task(conn, task)
+
+   print(pr_id, task_id)
+   conn.commit()
+
+   update(conn, "tasks", 2, status="started")
+
+   delete_where(conn, "tasks", id=3)
+   delete_all(conn, "tasks")
+
+   print(select_all(conn, "tasks"))
+   print(select_where(conn, "tasks",status="started"))
+   
